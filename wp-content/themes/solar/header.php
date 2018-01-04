2fd5ce19829f58ba18d04dd18aca4cd771600a68
Saulo Padilha <spadilha@spaMBP.local>
2018-01-04 19:39:25 -0200

    remove tag manager

--- wp-content/themes/solar/header.php
+++ wp-content/themes/solar/header.php
@@ -34,16 +34,6 @@
 </head>
 <body <?php body_class(); ?>>
 
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K9SM9R"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K9SM9R');</script>
-<!-- End Google Tag Manager -->
-
 <?php
 
     //Get active language to set the logo url path to home
